f876534b82b46619e99105cbf7ad517dc1f2c7f5
dkenw <dkenw1618@gmail.com>
2022-09-02 17:25:15 +0800

    feat: add goerli test tokens

--- validate.js
+++ validate.js
@@ -1,30 +1,30 @@
-const { schema } = require('@uniswap/token-lists');
-const Ajv = require('ajv');
-const addFormats = require('ajv-formats');
-const tokenlist = require('./tokenlist.json');
+const { schema } = require('@uniswap/token-lists')
+const Ajv = require('ajv')
+const addFormats = require('ajv-formats')
+const tokenlist = require('./tokenlist.json')
 
 function validate() {
-  const ajv = new Ajv({ allErrors: true, verbose: true });
-  addFormats(ajv);
-  const validator = ajv.compile(schema);
+  const ajv = new Ajv({ allErrors: true, verbose: true })
+  addFormats(ajv)
+  const validator = ajv.compile(schema)
 
-  const valid = validator(tokenlist);
+  const valid = validator(tokenlist)
   if (valid) {
-    console.log('valid\n');
+    console.log('valid\n')
   } else {
-    console.log(validator.errors);
+    console.log(validator.errors)
   }
 }
 
 function printMintable() {
   tokenlist.tokens.forEach((token) => {
     if (token.extensions && token.extensions.mintable && token.extensions.mintable.default) {
-      const amount = token.extensions.mintable.default;
-      const amountReadable = +amount / 10 ** token.decimals;
-      console.log(token.symbol, '\t', amountReadable);
+      const amount = token.extensions.mintable.default
+      const amountReadable = +amount / 10 ** token.decimals
+      console.log(token.chainId, '\t', token.address, '\t', token.symbol, '\t', amountReadable)
     }
-  });
+  })
 }
 
-validate();
-printMintable();
+validate()
+printMintable()
